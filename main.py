b2510930a4304d544192ed28fd15155233d1e3ad
ProgrammerX <mohammad66084@gmail.com>
2026-08-20 18:55:36 +0545

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,26 @@ except ModuleNotFoundError:
     time_taken = end_time - start_time
     print(f"[System]: Installed Dependencies in {time_taken:.3f}s")
 bot_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "bot.py")
-token = input("Enter your Bot Token: ")
-botName = input("Enter
+if os.path.exists(os.path.expanduser("~/Discord-Boost-Bot/.env")):
+    pass
+else:
+    token = input("Enter your Bot Token: ")
+    botName = input("Enter your Bot name (Default: HenixBot): ") or HenixBot
+    with open(os.path.expanduser("~/Discord-Boost-Bot/.env"), 'w') as f:
+        f.write(f"""# ---------------------------------------------------------------------------------------------------------------------------------------
+# Require Variables
+# ---------------------------------------------------------------------------------------------------------------------------------------
+
+# Replace with your Bot Token
+DISCORD_BOT_TOKEN=\"{token}\"
+
+# ---------------------------------------------------------------------------------------------------------------------------------------
+# Optional Variables
+# ---------------------------------------------------------------------------------------------------------------------------------------
+
+# Replace with your Bot's Name
+BOT_NAME=\"{botName}\"
+
+# Replace with your Database's File
+DATABASE_FILE="bot_data.json\"""")
 os.system(f'"{sys.executable}" "{bot_path}"')
