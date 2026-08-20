c4cd72111990be9b827a714066b90768a28b18fe
ProgrammerX <mohammad66084@gmail.com>
2026-08-20 18:38:26 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-import os, time, sys
+import os
+import time
+import sys
 
 try:
     import discord
 except ModuleNotFoundError:
-    print("[System]: Installing Dependencies...")    
-    start_time = time.perf_counter()    
-    os.system(f"{sys.executable} -m pip install -U discord.py")    
-    end_time = time.perf_counter()    
-    time_taken = end_time - start_time   
+    print("[System]: Installing Dependencies...")
+
+    start_time = time.perf_counter()
+
+    os.system(f"{sys.executable} -m pip install -U discord.py")
+
+    end_time = time.perf_counter()
+    time_taken = end_time - start_time
+
     print(f"[System]: Installed Dependencies in {time_taken:.3f}s")
-  
-os.system(f"{sys.executable} bot.py")
+
+bot_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "bot.py")
+
+os.system(f'"{sys.executable}" "{bot_path}"')
