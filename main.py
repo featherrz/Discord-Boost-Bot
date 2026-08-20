8ecce3af4d910c3a9bcb1f7196720d8bb65232f0
ProgrammerX <mohammad66084@gmail.com>
2026-08-20 18:42:07 +0545

    Update main.py

--- main.py
+++ main.py
@@ -6,16 +6,12 @@ try:
     import discord
 except ModuleNotFoundError:
     print("[System]: Installing Dependencies...")
-
     start_time = time.perf_counter()
-
     os.system(f"{sys.executable} -m pip install -U discord.py")
-
     end_time = time.perf_counter()
     time_taken = end_time - start_time
-
     print(f"[System]: Installed Dependencies in {time_taken:.3f}s")
-
 bot_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "bot.py")
-
+token = input("Enter your Bot Token: ")
+botName = input("Enter
 os.system(f'"{sys.executable}" "{bot_path}"')
